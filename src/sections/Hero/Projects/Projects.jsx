a7195912fdc4896fb48a49f555774bb2e1e1bc20
Dadrion <dtuircuit@gmail.com>
2024-09-09 18:14:48 -0500

    'updated project images'

--- src/sections/Hero/Projects/Projects.jsx
+++ src/sections/Hero/Projects/Projects.jsx
@@ -1,5 +1,5 @@
 import styles from './ProjectStyles.module.css'
-import Cloudformation from '../../../assets/Cloudformation.png';
+import Govtech from '../../../assets/Govtech.png';
 import JSprojectpic from '../../../assets/Reac Website.png';
 import Terraform  from '../../../assets/Terraform.png';
 import FoodDelivery from '../../../assets/Food Deliver Infrastructure.png';
@@ -11,28 +11,28 @@ function Projects() {
          <h1 className="sectionTitle"></h1>
          <div className={styles.projectsContainer} >
            <ProjectCard 
-           src={Cloudformation} 
-           link='https://github.com/dtuircuit/Cloud-Formation'
-           h3= "VPC CloudFormation" 
-           p="AWS Cloud"
+           src={Govtech} 
+           link='https://github.com/dtuircuit/Secure-and-Complaint-infrastructure-for-Government-Application'
+           h3= "Secure and Compliant Government Application Infrastructure " 
+           p="GovTech Infrastructure"
            />
            <ProjectCard 
            src={JSprojectpic} 
            link='https://github.com/dtuircuit/DT-React-Portfolio-Site'
            h3= "Portfolio React Site" 
-           p="JavaScript"
+           p="Dynamic Website"
            />
            <ProjectCard 
            src={Terraform} 
-           link='https://github.com/dtuircuit/terraform-portfolio-project/tree/master'
-           h3= "Next.js Terraform Deployment" 
-           p="AWS Cloud"
+           link='https://github.com/dtuircuit/Web_Application_Backend_Infra'
+           h3= "Secure Backend Application Infrastructure" 
+           p="Backend Infrastructure"
            />
            <ProjectCard 
            src={FoodDelivery} 
-           link='https://github.com/CloudFoodDelivery/order_app/tree/Dadrion'
-           h3= "Food Distribution Infrastructure" 
-           p="AWS Cloud"
+           link='https://github.com/CloudFoodDelivery/order_app'
+           h3= "Real-Time Food Distribution Infrastructure" 
+           p=""
            />
            
          </div>
